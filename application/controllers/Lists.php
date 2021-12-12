07fd1463dc394ddb2a83a6363a550452f362f79a
Dana Sugu <dana.sugu@gmail.com>
2021-12-12 13:49:59 +0200

    update

--- application/controllers/Lists.php
+++ application/controllers/Lists.php
@@ -1,7 +1,11 @@
 <?php
 class Lists extends CI_Controller {
+
+
+
     public function __construct() {
         parent::__construct();
+          $this->load->model('List_model');
         if(!$this->session->userdata('logged_in')){
             //Set error
             $this->session->set_flashdata('need_login', 'Sorry, you need to be logged in to view that area');
--- application/views/home.php
+++ application/views/home.php
@@ -1,33 +1,33 @@
+<!--Display Messages-->
 <?php if($this->session->flashdata('registered')) : ?>
-<p class="alert alert-dismissable alert-success">
-	<?php echo $this->session->flashdata('registered'); ?>
-</p>
+<?php echo '<p class="text-success">' .$this->session->flashdata('registered') . '</p>'; ?>
 <?php endif; ?>
-<?php if($this->session->flashdata('login_success')) : ?>
-<p class="alert alert-dismissable alert-success">
-	<?php echo $this->session->flashdata('login_success'); ?>
-</p>
+<?php if($this->session->flashdata('logged_out')) : ?>
+<?php echo '<p class="text-success">' .$this->session->flashdata('logged_out') . '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('need_login')) : ?>
+<?php echo '<p class="text-error">' .$this->session->flashdata('need_login') . '</p>'; ?>
 <?php endif; ?>
 
 <h1>Welcome to myTodo!</h1>
-<p>myTodo is a simple and helpful application to help you manage your day to day tasks. You can add as many task lists
+<p>myTodo us a simple and helpful application to help you manage your day to day tasks. You can add as many task lists
 	as you want and as many tasks as you want. myTodo is absolutely free! Have fun.</p>
 <?php if($this->session->userdata('logged_in')) : ?>
 <br />
 <!--Display Errors-->
-<!-- <?php echo validation_errors('<p class="text-error">'); ?> -->
+<?php echo validation_errors('<p class="text-error">'); ?>
 <p>
-	<!-- <?php echo form_open('lists/quickadd'); ?>
+	<?php echo form_open('lists/quickadd'); ?>
 	<?php $data = array("placeholder" => "Add a New List",
-                        "name" => "list_name"); ?>
-	<?php echo form_input($data); ?> -->
+                         "name" => "list_name"); ?>
+	<?php echo form_input($data); ?>
 	<!--Submit Buttons-->
-	<!-- <?php $data = array("value" => "Login",
+	<?php $data = array("value" => "Login",
                     "name" => "submit",
                     "style"=> "vertical-align:top;",
                     "class" => "btn btn-primary"); ?>
 	<?php echo form_submit($data); ?>
-	<?php echo form_close(); ?> -->
+	<?php echo form_close(); ?>
 </p>
 <br />
 <h3>My Latest Lists</h3>
@@ -37,15 +37,15 @@
 		<th>Created On</th>
 		<th>View</th>
 	</tr>
-	<!-- <?php if(isset($lists)) : ?>
-	<?php foreach($lists as $list) : ?> -->
-	<!-- <tr>
+	<?php if(isset($lists)) : ?>
+	<?php foreach($lists as $list) : ?>
+	<tr>
 		<td><?php echo $list->list_name; ?></td>
 		<td><?php echo $list->create_date; ?></td>
 		<td><a href="<?php echo base_url(); ?>lists/show/<?php echo $list->id; ?>">View List</a></td>
 	</tr>
 	<?php endforeach; ?>
-	<?php endif; ?> -->
+	<?php endif; ?>
 </table>
 
 <h3>Latest Tasks</h3>
