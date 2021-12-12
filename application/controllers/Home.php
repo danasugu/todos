b97e97f1c5b41973ad80fe6a8f1fc18c80c3e633
Dana Sugu <dana.sugu@gmail.com>
2021-12-12 10:54:32 +0200

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,18 +2,41 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// class Home extends CI_Controller {
+
+//   public function index()
+//   {
+//     // $data['welcome'] = 'welcome';
+//     // $this->load->view('home', $data);
+//     $data['main_content'] = 'home';
+
+//     $this->load->view('layouts/main', $data);
+
+
+//   }
+
+// }
+
 class Home extends CI_Controller {
+    public function __construct()
+    {
+    parent::__construct();
+    $this->load->model('List_model');
+    $this->load->model('Task_model');
 
-  public function index()
-  {
-    // $data['welcome'] = 'welcome';
-    // $this->load->view('home', $data);
-    $data['main_content'] = 'home';
+    }
+    public function index(){
+        if($this->session->userdata('logged_in')){
+            //Get the logged in users id
+            $user_id = $this->session->userdata('user_id');
+            //Get all lists from the model
+            $data['lists'] = $this->List_model->get_all_lists($user_id);
+            $data['tasks'] = $this->Task_model->get_users_tasks($user_id);
+        }
 
-    $this->load->view('layouts/main', $data);
-
-
-  }
+        $data['main_content'] = 'home';
+        $this->load->view('layouts/main',$data);
+    }
 
 }
 
